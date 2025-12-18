db255d403287be93e64fb0dbc99a4fa6886f66a8
Nolan Brady <nolan.brady@colorado.edu>
2025-12-18 09:34:33 -0700

    Add the recall assessment to the combined dataset.

--- generate_combined_data.py
+++ generate_combined_data.py
@@ -16,9 +16,11 @@ import pandas as pd
 
 engagement_data = pd.read_csv("./data/tabular/engagement_data_processed.csv")
 socio_demographic_data = pd.read_csv("./data/tabular/socio_demographic_data_processed.csv")
+recall_assessment_data = pd.read_csv("./data/tabular/recall_assessment_score_diffs.csv")
 
 # Merge the data on the subject_id column
 combined_data = pd.merge(engagement_data, socio_demographic_data, on="subject_id", how="inner")
+combined_data = pd.merge(combined_data, recall_assessment_data, on="subject_id", how="inner")
 
 # Save the combined data
 combined_data.to_csv("./data/tabular/combined_sfv_data.csv", index=False)
